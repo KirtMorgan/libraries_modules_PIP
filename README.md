7f2892e292ed05ebf8ce2b204737e49801eea177
Kirt Morgan <kirtmorgan@live.com>
2019-04-24 11:54:28 +0100

    Tested and used standard libraries in python, understanding the random module

---
+++ README.md
@@ -0,0 +1,2 @@
+# Libraries, Modules and PIP
+To understand and practice the uses of libraries, modules and PIP
---
+++ py_lib.py
@@ -0,0 +1,8 @@
+from random import *
+# from random import random, randint
+
+print(random())
+print(randint(0, 100))
+print(type(random))
+print(sample(['person1', 'person2', 'person3'], 3))
+print(choice(['person1', 'person2', 'person3']))
